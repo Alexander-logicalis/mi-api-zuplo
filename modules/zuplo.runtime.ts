80df0ea2871a4f8f86cf6112b3a14287097110db
Alexander Logicalis <logicalisa@gmail.com>
2026-01-30 14:48:23 -0600

    222

--- modules/zuplo.runtime.ts
+++ modules/zuplo.runtime.ts
@@ -9,13 +9,13 @@ import {
 export function runtimeInit(runtime: RuntimeExtensions) {
   runtime.addPlugin(
     new AkamaiApiSecurityPlugin({
-      hostname: "https://att-mexico.nonamesec.com/engine",
+      hostname: "att-mexico.nonamesec.com",
       // index, provided by Akamai API Security
       index: 2,
       // Key provided by Akamai API Security
       key: environment.AKAMAI_API_SECURITY_KEY,
       // Enable the active prevention/protection feature
-      enableProtection: false,
+      enableProtection: true,
     }),
   );
 }
